681bb2eaf219b1fd8bf3b5ca919d4babae1bf2f4
mashiike <ikeda-masashi@kayac.com>
2022-12-16 03:23:39 +0900

    iam role for redshift

--- lambda/example.tf
+++ lambda/example.tf
@@ -21,3 +21,27 @@ resource "aws_iam_role_policy_attachment" "basic" {
   role       = aws_iam_role.lambda.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+
+resource "aws_iam_role" "redshift" {
+  name = "lambda-udf-redshift"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "redshift.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "redshift" {
+  role       = aws_iam_role.redshift.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaRole"
+}
